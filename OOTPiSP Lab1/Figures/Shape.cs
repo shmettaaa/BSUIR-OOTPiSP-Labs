ff6f0587cadaa64b139833756a2c9c25744140e1
shmettaaa <yavayhyh@gmail.com>
2026-02-28 22:19:44 +0300

    Add project files.

---
+++ OOTPiSP Lab1/Figures/Shape.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Figures
+{
+    public abstract class Shape
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Brush Stroke { get; set; }
+        public double StrokeThickness { get; set; }
+
+        protected Shape()
+        {
+            X = 0;
+            Y = 0;
+            Stroke = Brushes.Black;
+            StrokeThickness = 2.0;
+        }
+
+        protected Shape(double x, double y)
+        {
+            X = x;
+            Y = y;
+            Stroke = Brushes.Black;
+            StrokeThickness = 2.0;
+        }
+
+        protected Shape(double x, double y, Brush stroke, double thickness)
+        {
+            X = x;
+            Y = y;
+            Stroke = stroke ?? Brushes.Black;
+            StrokeThickness = thickness > 0 ? thickness : 2.0;
+        }
+
+        public abstract void Draw(Canvas canvas);
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} at ({X:F1}, {Y:F1})";
+        }
+    }
+}
